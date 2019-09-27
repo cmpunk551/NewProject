4c1e8daf4735400b941791487e5fcc7eac43e961
Nikolay Chernyak <cmpunk5551@gmail.com>
2019-09-27 21:51:24 +0300

    принятие json.3

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -32,7 +32,7 @@ class TodosController < ApplicationController
   def create
     if (params[:todo])
       puts("Я ИЗ ВЕБА АЛОООООО")
-      puts(params[:todo])
+      puts(params[:todo].has_key?(:text))
       @todo = Todo.new(todo_params)
       @todo.project_id = params[:project_id]
     else
